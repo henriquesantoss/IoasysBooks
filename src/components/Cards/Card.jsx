c75fe1ed834d839f7bac159c67ad32c9b0372e21
henriquesantoss <henriiqueacs@gmail.com>
2022-06-14 09:31:11 -0300

    feat: adding cards

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -2,29 +2,31 @@ import * as S from './Card.Style'
 import background from '../../assets/background/book.jpg'
 
 const Card = ({
-    book: {
-        authors, publisher, title, pageCount, imageUrl, published,
-    },
+  onClick,
+  book: { authors, publisher, title, pageCount, imageUrl, published },
 }) => (
-    <S.Container>
-        <S.ImageContainer>
-            {
-                <S.Image src={imageUrl ? imageUrl : background} width={82} height={122} alt={title} />
-            }
-        </S.ImageContainer>
+  <S.Container onClick={onClick}>
+    <S.ImageContainer>
+      {
+        <S.Image
+          src={imageUrl ? imageUrl : background}
+          width={82}
+          height={122}
+          alt={title}
+        />
+      }
+    </S.ImageContainer>
 
-        <div className="content">
-            <S.Title>
-                {title ?? 'Livro desconhecido'}
-            </S.Title>
-            <S.Author>
-                {authors ?? 'Autor desconhecido'}
-            </S.Author>
-            <S.Description>
-                {pageCount ? "paginas" : "Quantidade de paginas desconhecidas"}
-            </S.Description>
-        </div>
-    </S.Container>
+    <div className="content">
+      <S.Title>{title ?? 'Livro desconhecido'}</S.Title>
+      <S.Author>{authors[0] ?? 'Autor desconhecido'}</S.Author>
+      <S.Description>
+        <span> {pageCount ?? 'Numero de paginas desconhecidas'} páginas</span>
+        <span> {publisher ?? 'Editora desconhecida'} </span>
+        <span> Publicado em {published ?? 'Publicação desconhecida'}</span>
+      </S.Description>
+    </div>
+  </S.Container>
 )
 
-export default Card;
+export default Card

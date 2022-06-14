467b3c8f56148e7dfd09e526971302a76ea1bce2
henriquesantoss <henriiqueacs@gmail.com>
2022-06-14 09:31:24 -0300

    feat: adding headers

--- src/components/Header/Header.Style.js
+++ src/components/Header/Header.Style.js
@@ -1,37 +1,45 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    h1 {
-        display: none;
-    }
+  h1 {
+    display: none;
+  }
 
-    span {
-        font-size: 28px;
-        font-weight: 300;
-        line-height: 40px;
-        margin-left: 16px;
-        color: ${({ theme }) => theme.colors.white};
-    }
-`;
+  span {
+    font-size: 28px;
+    font-weight: 300;
+    line-height: 40px;
+    margin-left: 16px;
+    color: ${({ theme }) => theme.colors.white};
+  }
+`
+
+export const User = styled.div`
+  font-size: 28px;
+  font-weight: 300;
+  line-height: 40px;
+  margin-right: 16px;
+  color: ${({ theme }) => theme.colors.white};
+`
 
 export const Logo = styled.div`
-    display: flex;
-    align-items: center;
-    margin: 50px 50px;
+  display: flex;
+  align-items: center;
+  margin: 50px 50px;
 
-    ${({mode, theme}) =>
-    mode === 'dark' && css`
-        span{
-            color: ${theme.colors.dark};
-        }
+  ${({ mode, theme }) =>
+    mode === 'dark' &&
+    css`
+      span {
+        color: ${theme.colors.dark};
+      }
 
-        svg path {
-            fill: ${theme.colors.dark}
-        }
-    `
-}
-`;
+      svg path {
+        fill: ${theme.colors.dark};
+      }
+    `}
+`
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,22 @@
- import { useState } from 'react';
- import * as S from './Header.Style';
- import LogoWhite from '../../assets/logo/white.svg';
- import LogoBlack from '../../assets/logo/black.svg';
+import { useState } from 'react'
+import * as S from './Header.Style'
+import LogoWhite from '../../assets/logo/white.svg'
+import LogoBlack from '../../assets/logo/black.svg'
+import user from '../../mocks/user.json'
+const Header = ({ mode = 'light', user = null }) => {
+  const [actualMode] = useState(mode || 'light')
 
- const Header = ({ mode = 'light', user = null  }) => {
-    const [actualMode] = useState(mode || 'light');
+  return (
+    <S.Container>
+      <S.Logo mode={actualMode}>
+        <img src={actualMode === 'dark' ? LogoBlack : LogoWhite} alt="ioasys" />
+        <span>Books</span>
+      </S.Logo>
+      <S.User>
+        <span>Bem vindo Doutor(a) {user}</span>
+      </S.User>
+    </S.Container>
+  )
+}
 
-    return (
-        <S.Container>
-            <S.Logo mode={actualMode}>
-                <img src={actualMode === 'dark' ? LogoBlack : LogoWhite} alt="ioasys" />
-                <span>Books</span>
-            </S.Logo>
-        </S.Container>
-    )
- }
-     
- 
-
- export default Header;
+export default Header
